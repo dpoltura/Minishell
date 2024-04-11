7435ecb06b4b297fac147c9ae3cfdb29d5f4a3ba
dpoltura <dpoltura@2G5.42angouleme.fr>
2024-04-11 17:32:16 +0200

    merge main

--- minishell_parse/minishell_parse.c
+++ minishell_parse/minishell_parse.c
@@ -3,18 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell_parse.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aduriez <aduriez@student.42.fr>            +#+  +:+       +#+        */
+/*   By: dpoltura <dpoltura@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/09 17:08:04 by dpoltura          #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2024/04/11 16:17:40 by aduriez          ###   ########.fr       */
-=======
-<<<<<<< HEAD
-/*   Updated: 2024/04/11 15:22:51 by dpoltura         ###   ########.fr       */
-=======
 /*   Updated: 2024/04/11 16:45:33 by dpoltura         ###   ########.fr       */
->>>>>>> Donovan
->>>>>>> refs/remotes/origin/main
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,20 +14,11 @@
 
 static void	print_arg(t_data *data)
 {
-	int	i;
+	int		i;
 
 	i = 0;
 	if (!data || !data->arg)
 		return ;
-<<<<<<< HEAD
-	printf("[ ");
-	while (data->arg[i])
-	{
-		printf("%s ", data->arg[i]);
-		i++;
-	}
-	printf("]");
-=======
 	printf(ANSI_BLUE"char "ANSI_RED"**"ANSI_CYAN"arg "ANSI_RED"= "ANSI_WHITE);
 	while (data->arg[i])
 	{
@@ -47,7 +30,6 @@ static void	print_arg(t_data *data)
 			printf(";");
 	}
 	printf("\n\t");
->>>>>>> Donovan
 }
 
 static char	*print_token(int token)
@@ -60,39 +42,39 @@ static char	*print_token(int token)
 		name = strdup("S_QUOTE");
 	if (token == 2)
 		name = strdup("D_QUOTE");
-	if (token == 3)
+    if (token == 3)
 		name = strdup("CMD");
-	if (token == 4)
+    if (token == 4)
 		name = strdup("PIPE");
-	if (token == 5)
+    if (token == 5)
 		name = strdup("L_CHEV");
-	if (token == 6)
+    if (token == 6)
 		name = strdup("R_CHEV");
 	if (token == 7)
 		name = strdup("D_L_CHEV");
-	if (token == 8)
+    if (token == 8)
 		name = strdup("D_R_CHEV");
-	if (token == 9)
+    if (token == 9)
 		name = strdup("DOLLAR");
 	if (token == 10)
 		name = strdup("ARG");
-	if (token == 11)
+    if (token == 11)
 		name = strdup("ECHO");
 	if (token == 12)
 		name = strdup("CD");
 	if (token == 13)
 		name = strdup("PWD");
-	if (token == 14)
+    if (token == 14)
 		name = strdup("EXPORT");
-	if (token == 15)
+    if (token == 15)
 		name = strdup("UNSET");
 	if (token == 16)
 		name = strdup("ENV");
-	if (token == 17)
+    if (token == 17)
 		name = strdup("EXIT");
 	if (token == 18)
 		name = strdup("INFILE");
-	if (token == 19)
+    if (token == 19)
 		name = strdup("OUTFILE");
 	return (name);
 }
@@ -101,17 +83,11 @@ static void	print_data(t_data *data)
 {
 	char	*token;
 	int		i;
-
+	
 	i = 0;
 	while (data)
 	{
 		token = print_token(data->token);
-<<<<<<< HEAD
-		printf("[ %d ][ %s ]", data->index, data->value);
-		print_arg(data);
-		printf("[ %s ]", token);
-		printf("[ %s ]\n", data->path);
-=======
 		printf(ANSI_RED"\ntypedef "ANSI_BLUE"struct\t"ANSI_GREEN"s_data\n");
 		printf(ANSI_YELLOW"{");
 		printf("\n\t"ANSI_BLUE"int "ANSI_CYAN"index "ANSI_RED"= "ANSI_WHITE"%d;\n\t", data->index);
@@ -120,7 +96,6 @@ static void	print_data(t_data *data)
 		printf(ANSI_GREEN"t_token "ANSI_CYAN"token "ANSI_RED"= "ANSI_WHITE"%s;\n\t", token);
 		printf(ANSI_BLUE"char "ANSI_RED"*"ANSI_CYAN"path "ANSI_RED"= "ANSI_WHITE"%s;\n", data->path);
 		printf(ANSI_YELLOW"}\n"ANSI_WHITE);
->>>>>>> Donovan
 		data = data->next;
 		if (!data)
 			printf("\n");
@@ -129,13 +104,11 @@ static void	print_data(t_data *data)
 	}
 }
 
-int	main(int arc, char *arg[], char *envp[])
+int		main(void)
 {
 	t_data	*data;
 	char	*input;
 
-	(void)arc;
-	(void)arg;
 	data = NULL;
 	while (1)
 	{
@@ -154,8 +127,8 @@ int	main(int arc, char *arg[], char *envp[])
 		get_path(data);
 		token_infile(data);
 		token_outfile(data);
+		
 		print_data(data);
-		ft_execve(data, envp);
 		free_data(&data);
 	}
 	return (0);
--- minishell_parse/minishell_parse.h
+++ minishell_parse/minishell_parse.h
@@ -3,102 +3,73 @@
 /*                                                        :::      ::::::::   */
 /*   minishell_parse.h                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aduriez <aduriez@student.42.fr>            +#+  +:+       +#+        */
+/*   By: dpoltura <dpoltura@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/09 17:05:05 by dpoltura          #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2024/04/11 16:51:38 by aduriez          ###   ########.fr       */
-=======
-<<<<<<< HEAD
-/*   Updated: 2024/04/11 14:56:31 by dpoltura         ###   ########.fr       */
-=======
 /*   Updated: 2024/04/11 16:25:21 by dpoltura         ###   ########.fr       */
->>>>>>> Donovan
->>>>>>> refs/remotes/origin/main
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_PARSE_H
 # define MINISHELL_PARSE_H
 
-<<<<<<< HEAD
-# include <fcntl.h>
-=======
 # include "colors.h"
 # include <unistd.h>
 # include <stdlib.h>
 # include <stdio.h>
 # include <readline/readline.h>
->>>>>>> refs/remotes/origin/main
 # include <readline/history.h>
-# include <readline/readline.h>
-# include <stdio.h>
-# include <stdlib.h>
-# include <sys/wait.h>
-# include <unistd.h>
 
-typedef enum e_token
+typedef enum	e_token
 {
-	EMPTY,
+    EMPTY,
 	S_QUOTE,
 	D_QUOTE,
-	CMD,
-	PIPE,
-	L_CHEV,
-	R_CHEV,
-	D_L_CHEV,
-	D_R_CHEV,
-	DOLLAR,
-	ARG,
-	ECHO,
-	CD,
-	PWD,
-	EXPORT,
-	UNSET,
-	ENV,
-	EXIT,
-	INFILE,
-	OUTFILE,
-}					t_token;
+    CMD,
+    PIPE,
+    L_CHEV,
+    R_CHEV,
+    D_L_CHEV,
+    D_R_CHEV,
+    DOLLAR,
+    ARG,
+    ECHO,
+    CD,
+    PWD,
+    EXPORT,
+    UNSET,
+    ENV,
+    EXIT,
+    INFILE,
+    OUTFILE,
+}	t_token;
 
-typedef struct s_data
+typedef struct	s_data
 {
-	int				index;
-	char			*value;
-	char			**arg;
-	t_token			token;
-	char			*path;
-	struct s_data	*prev;
-	struct s_data	*next;
+    int		index;
+    char	*value;
+    char	**arg;
+    t_token	token;
+    char    *path;
+    struct s_data	*prev;
+    struct s_data	*next;
+}	t_data;
 
-}					t_data;
-
-typedef struct s_excev
-{
-	int				pipe_filled;
-	int				open_outfile;
-	int				open_file;
-	int				fd[2];
-	int				fd_origin[2];
-}					t_excev;
-
-void				init_data(t_data **data);
-void				split_input(char *input, t_data *data);
-void				free_data(t_data **data);
-void				index_data(t_data *data);
-void				token_data(t_data *data);
-int					ft_strcmp(char *src, char *cmp);
-char				*ft_strdup(const char *s);
-void				arg_to_cmd(t_data *data);
-size_t				ft_strlen(const char *s);
-char				**ft_split(char const *s, char c);
-char				*ft_substr(char const *s, unsigned int start, size_t len);
-char				*ft_strjoin(char const *s1, char const *s2);
-void				get_path(t_data *data);
-void				free_split(char **split);
-void				token_infile(t_data *data);
-void				token_outfile(t_data *data);
-void				ft_execve(t_data *data, char *envp[]);
-void				ft_init_open_file(t_excev *base, t_data *data);
+void	init_data(t_data **data);
+void	split_input(char *input, t_data *data);
+void	free_data(t_data **data);
+void	index_data(t_data *data);
+void	token_data(t_data *data);
+int		ft_strcmp(char *src, char *cmp);
+char    *ft_strdup(const char *s);
+void	arg_to_cmd(t_data *data);
+size_t  ft_strlen(const char *s);
+char	**ft_split(char const *s, char c);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);
+void    get_path(t_data *data);
+void	free_split(char **split);
+void	token_infile(t_data *data);
+void	token_outfile(t_data *data);
 
 #endif
